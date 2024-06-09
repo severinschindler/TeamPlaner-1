5f3ee07f8334ea65e78eeff2599536a53393eada
Severin Schindler <severinschindler@gmail.com>
2024-06-09 17:53:37 +0200

    adjsut year and group for 24

--- teamplaner/www/swissunihockey.py
+++ teamplaner/www/swissunihockey.py
@@ -17,7 +17,7 @@ def get_context(context):
 	return context
 
 def get_spielplan():
-	response = requests.get("https://api-v2.swissunihockey.ch/api/games?mode=team&team_id=428691&season=2023&games_per_page=20")
+	response = requests.get("https://api-v2.swissunihockey.ch/api/games?mode=team&team_id=428691&season=2024&games_per_page=20")
 	r = response.json()['data']
 	data = {}
 	data['titel'] = r['title']
@@ -25,9 +25,9 @@ def get_spielplan():
 	for game in r['regions'][0]['rows']:
 		data['games'].append(game)
 	return data
-	
+
 def get_tabelle():
-	response = requests.get("https://api-v2.swissunihockey.ch/api/rankings?season=2023&league=5&game_class=11&group=415797&view=full")
+	response = requests.get("https://api-v2.swissunihockey.ch/api/rankings?season=2024&league=6&game_class=11&group=416220&view=full")
 	r = response.json()['data']
 	data = {}
 	data['titel'] = r['title']
@@ -38,5 +38,5 @@ def get_tabelle():
 	return data
 
 # Anleitung für Group ID:
-# Das Jahr ändern der folgenden API: https://api-v2.swissunihockey.ch/api/games?mode=team&team_id=428691&season=2022&games_per_page=20 und im 
-# Browser eingeben. Im Browser nach "group" suchen und die Group ID auslesen.
+# Das Jahr ändern der folgenden API: https://api-v2.swissunihockey.ch/api/games?mode=team&team_id=428691&season=2022&games_per_page=20 und im
+# Browser eingeben. Im Browser nach "group" suchen und die Group ID auslesen und in der zweiten API anpassen.
